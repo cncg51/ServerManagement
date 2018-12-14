ee91a45c8d37315f74a8bb5b1b6626dd4a4eec93
Michael chen <cncg51@gmail.com>
2018-12-14 12:18:10 +0800

    .....

--- static/js/common.js
+++ static/js/common.js
@@ -1,29 +1,29 @@
-layui.define(['layer'], function(exports) {
-	"use strict";
+layui.define(['layer'], function (exports) {
+  'use strict'
 
-	var $ = layui.jquery,
-		layer = layui.layer;
+  var $ = layui.jquery,
+    layer = layui.layer
 
-	var common = {
-		/**
-		 * 抛出一个异常错误信息
-		 * @param {String} msg
-		 */
-		throwError: function(msg) {
-			throw new Error(msg);
-			return;
-		},
-		/**
-		 * 弹出一个错误提示
-		 * @param {String} msg
-		 */
-		msgError: function(msg) {
-			layer.msg(msg, {
-				icon: 5
-			});
-			return;
-		}
-	};
+  var common = {
+    /**
+     * 抛出一个异常错误信息
+     * @param {String} msg
+     */
+    throwError: function (msg) {
+      throw new Error(msg)
+      return
+    },
+    /**
+     * 弹出一个错误提示
+     * @param {String} msg
+     */
+    msgError: function (msg) {
+      layer.msg(msg, {
+        icon: 5
+      })
+      return
+    }
+  }
 
-	exports('common', common);
-});
+  exports('common', common)
+})
